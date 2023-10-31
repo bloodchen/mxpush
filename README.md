39727e75a3189fe775f11728abf66ae14828865d
bloodchen <bloodchen@gmail.com>
2023-10-31 14:27:00 +0800

    update

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 realtime push server
 
+## Quick Start
+
+For client
+
+1. call /mxpush/url to get the usable push server url, if got 'this', just use current server
+2. call /mxpush/connect to connect to the server
+
+For Server
+
+1. call /mxpush/post to post data to one or more clients
+
 ## API
 
 ### /mxpush/url
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import fastifyModule from 'fastify';
+import crypto from 'crypto';
 import dotenv from "dotenv";
 import pkg from "better-sse";
+
 import { config } from './config.js'
 
+const tokenPass = "2rnma5xsctJhx1Z$#%^09FYkRfuAsxTB"
 const { createSession } = pkg;
 
 export const app = fastifyModule({ logger: false });
@@ -11,6 +14,29 @@ async function startServer() {
     const port = process.env.port || 8080
     await app.listen({ port, host: '0.0.0.0' });
     console.log("Starting mxpush service on:", port)
+}
+function decrypt({ data, password, from_encoding = 'hex', to_encoding = 'utf8', length = 256 }) {
+    try {
+        const buf = Buffer.from(data, from_encoding)
+        var iv = buf.subarray(0, 16)
+        var algorithm = `aes-${length}-cbc`;
+        var decipher = crypto.createDecipheriv(algorithm, Buffer.from(password), iv)
+        var decrypted = Buffer.concat([decipher.update(buf.subarray(16)), decipher.final()]);
+        return decrypted.toString(to_encoding);
+    } catch (e) {
+        return null
+    }
+
+}
+function userFromToken({ token }) {
+    try {
+        const data = decrypt({ data: token, password: tokenPass, from_encoding: "base64" })
+        const user = JSON.parse(data)
+        return user || {}
+    } catch (e) {
+        console.error(e.message)
+    }
+    return {}
 }
 dotenv.config()
 startServer()
@@ -23,9 +49,12 @@ app.get('/mxpush/status', async (req, res) => {
     return { count }
 })
 app.get('/mxpush/connect', async (req, res) => {
-    const { uid } = req.query
+    const { uid, token, type = 'mx' } = req.query
     if (!uid) return { code: 100, msg: 'uid is missing' }
     if (clients[uid]) return { code: 101, msg: 'already connected' }
+    const { user_id } = userFromToken({ token })
+    if (uid != user_id) return { code: 102, msg: 'invalid user' }
+
     console.log(uid, "connected. total = ", ++count)
     const eventName = process.env.eventName || 'mxpush'
     const session = await createSession(req.raw, res.raw, { headers: { "Access-Control-Allow-Origin": '*' } })
