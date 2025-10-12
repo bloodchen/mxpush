d141aff2189f3183245bdbb7be56c60b9b730f16
bloodchen <bloodchen@gmail.com>
2025-10-12 10:30:36 +0800

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 source .env
+echo "📡 创建 Docker 网络（如果不存在）..."
+docker network inspect mxnet >/dev/null 2>&1 || \
+docker network create mxnet
+
 NAME="mxpush"
-PORT="${dockerPort:=8100}"
+
 docker rm -f $NAME >/dev/null 2>&1 || true
 docker build -t $NAME .
 ##mkdir data && chmod a+rw data
-docker run --name $NAME --ulimit nofile=1048576:1048576 \
+docker run --name $NAME --ulimit nofile=1048576:1048576 --network mxnet \
     --log-driver json-file --log-opt max-size=200m --log-opt max-file=3 \
-    -p $PORT:8080 --restart=always -d $NAME
+    --restart=always -d $NAME
--- caddy.sh
+++ caddy.sh
@@ -1,8 +1,12 @@
+
+echo "📡 创建 Docker 网络（如果不存在）..."
+docker network inspect mxnet >/dev/null 2>&1 || \
+docker network create mxnet
+
 docker rm -f caddy >/dev/null 2>&1 || true
 
 docker run --name caddy -d -p 80:80 -p 443:443 \
     -v $PWD/www:/usr/share/caddy \
-    -v $PWD/Caddyfile:/etc/caddy/Caddyfile \
+    -v $PWD/Caddyfile:/etc/caddy/Caddyfile --network mxnet \
     -v caddy_data:/data \
-    --network mxnet \
     --restart=always caddy
