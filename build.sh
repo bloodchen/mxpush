b9b9b91d307fe1bce51268aa72de21e256042917
bloodchen <bloodchen@gmail.com>
2025-10-12 21:27:39 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-source .env
 echo "📡 创建 Docker 网络（如果不存在）..."
 docker network inspect mxnet >/dev/null 2>&1 || \
 docker network create mxnet
@@ -8,6 +7,6 @@ NAME="mxpush"
 docker rm -f $NAME >/dev/null 2>&1 || true
 docker build -t $NAME .
 ##mkdir data && chmod a+rw data
-docker run --name $NAME --ulimit nofile=1048576:1048576 -p 8100:8080 --network mxnet \
+docker run --name $NAME --ulimit --env-file env nofile=1048576:1048576 -p 8100:8080 --network mxnet \
     --log-driver json-file --log-opt max-size=200m --log-opt max-file=3 \
     --restart=always -d $NAME
